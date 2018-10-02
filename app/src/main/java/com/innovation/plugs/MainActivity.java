15aeae9731bcce61c165b6f37bb441c1a5f4d5c8
denegra <denegra@gmail.com>
2018-10-01 23:19:51 -0400

    Error while fetching schemas.android

--- app/src/main/java/com/innovation/plugs/MainActivity.java
+++ app/src/main/java/com/innovation/plugs/MainActivity.java
@@ -7,11 +7,22 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnOnOff, btnDiscovery, btnSend;
     ListView listView;
-    TextView read_msg_box, ConnectionStatus;
+    TextView read_msg_box, connectionStatus;
     EditText writeMSg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialWork();
+    }
+
+    private void initialWork() {
+        btnOnOff=(Button) findViewById(R.id.onOff);
+        btnDiscovery=(Button) findViewById(R.id.discover);
+        btnSend=(Button) findViewById(R.id.sendButton);
+        listView=(ListView) findViewById(R.id.peerListView);
+        read_msg_box=(TextView) findViewById(R.id.readMsg);
+        connectionStatus=(TextView) findViewById(R.id.connectionStatus);
+        writeMSg=(EditText) findViewById(R.id.writeMsg);
     }
 }
